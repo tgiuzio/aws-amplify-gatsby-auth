6c0c53ada123ab83b73aa768098a42dfe993c4b7
tgiuzio <tgiuzio@me.com>
2021-07-17 15:35:40 -0300

    up index

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,6 +35,7 @@ const Layout = ({ children, data }) => (
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
+            backgroundColor:5,
           }}
         >
           {children}
