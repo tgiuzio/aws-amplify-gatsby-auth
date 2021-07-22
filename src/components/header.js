ca6723354edba6ed6ef2dc0f4da51a2901a318b4
Toni Giuzio <tgiuzio@me.com>
2021-07-22 17:32:06 -0300

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-      <h3><Link to="/app/login">LogIn</Link><br /></h3>
+      <h3><Link to="/app/login">Login</Link><br /></h3>
       {
         isLoggedIn() && (
           <p
