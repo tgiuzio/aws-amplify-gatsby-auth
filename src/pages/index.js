79d72f398f902d628eadc6025d9ef9adb76336a5
tgiuzio <tgiuzio@me.com>
2021-07-23 18:18:11 -0300

    inswx

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,9 @@ Amplify.configure(config)
 
 const IndexPage = () => (
   <Layout>
-    <div class="col-md-6"><img class="img-thumbnail" src="images/rapton.png"></div>
     <h1>Hi there</h1>
     <p>Rapton3D Ecosystem. <a href="https://www.rapton3d.com">Site</a></p>
     <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>  
-
     <Link to="/app/login">Sign In</Link><br />
     <Link to="/app/home">Home</Link><br />
     <Link to="/app/profile">Your profile</Link>
