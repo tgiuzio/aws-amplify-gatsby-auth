e93d7c1d242f07b9d6a7a8885fd600b4bc1567bd
tgiuzio <tgiuzio@me.com>
2021-07-23 18:13:01 -0300

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const IndexPage = () => (
     <h1>Hi there</h1>
     <p>Rapton3D Ecosystem. <a href="https://www.rapton3d.com">Site</a></p>
     <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>  
+
     <Link to="/app/login">Sign In</Link><br />
     <Link to="/app/home">Home</Link><br />
     <Link to="/app/profile">Your profile</Link>
