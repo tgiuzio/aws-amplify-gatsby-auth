abcf088f0bbfe7fc22776a150ebf979e18b6a9b8
tgiuzio <tgiuzio@me.com>
2021-07-27 14:13:24 -0300

    header

--- src/components/header.js
+++ src/components/header.js
@@ -30,6 +30,12 @@ const Header = ({ siteTitle }) => (
         </Link>
         
       </h1>
+      <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
       <h3 style={{ margin: 0 }}>
         <Link to="/app/login">Login</Link><br /></h3>
      
